fb911055bb7381d4965908f84e391e6f1af8a0ea
FACTS  LIMITED <admin@factsafrica.com>
2020-08-07 15:54:13 +0300

    complete all CRUD Operations for the products

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,126 +1,172 @@
-const { validationResult } = require('express-validator')
-const HttpError = require('../models/http-error')
-const Product = require('../models/products')
+const { validationResult } = require('express-validator');
+const HttpError = require('../models/http-error');
+const Product = require('../models/products');
 
-  //CREATE
+//CREATE
 
-  const createProduct = async (req,res,next) => {
-    const errors = validationResult(req)
+const createProduct = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-      throw new HttpError('Invalid Inputs! Check your data',422)
-    }
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid Inputs! Check your data', 422);
+  }
 
-    const { name,description,imageUrls,sizes,colors,price,creator } = req.body
+  const {
+    name,
+    description,
+    imageUrls,
+    sizes,
+    colors,
+    price,
+    creator,
+  } = req.body;
 
-    // const createdProduct = {
-    //    name,description,images,sizes,colors,price,creator
-    // }
-    let result
-    const createdProduct = new Product({name,description,imageUrls,sizes,colors,price,creator})
-    try {
-      result = await createdProduct.save()
-    } catch (error) {
-      return next(new HttpError('Fail to save product',400))
-    }
+  // const createdProduct = {
+  //    name,description,images,sizes,colors,price,creator
+  // }
+  let result;
+  const createdProduct = new Product({
+    name,
+    description,
+    imageUrls,
+    sizes,
+    colors,
+    price,
+    creator,
+  });
+  try {
+    result = await createdProduct.save();
+  } catch (error) {
+    return next(new HttpError('Fail to save product', 400));
+  }
 
-    res.status(201).json({message: 'Product Created', product:result}) 
-}
+  res.status(201).json({ message: 'Product Created', product: result });
+};
 
 //READ
 
-const allProducts  = async (req, res, next) => {
-    const fetchedProducts = await Product.find().exec()
-    res.status(200).json({totalProducts: fetchedProducts.length, products: fetchedProducts})
+const allProducts = async (req, res, next) => {
+  const fetchedProducts = await Product.find().exec();
+  res
+    .status(200)
+    .json({ totalProducts: fetchedProducts.length, products: fetchedProducts });
+};
+
+const productsById = async (req, res, next) => {
+  const productId = req.params.prodId;
+  let foundProduct;
+  try {
+    foundProduct = await Product.findById(productId).exec();
+  } catch (error) {
+    return next(new HttpError('Fetch product failed', 500));
+  }
+  if (!foundProduct) {
+    return next(
+      new HttpError('COuld not find product for the provided ID', 404)
+    );
+  }
+  res
+    .status(200)
+    .json({
+      message: 'Product Found',
+      product: foundProduct.toObject({ getters: true }),
+    });
+};
+
+const productsByUserId = async (req, res, next) => {
+  const userId = req.params.userId;
+  let foundProducts;
+
+  try {
+    foundProducts = await Product.find({ creator: userId }).exec();
+  } catch (error) {
+    return next(new HttpError('Fetch products failed', 500));
   }
 
-const productsById = async (req,res,next) => {
-    const productId = req.params.prodId
-    let foundProduct
-    try {
-      foundProduct = await Product.findById(productId).exec()
-    } catch (error) {
-      return next(new HttpError('Fetch product failed',500))
-    } 
-    if(!foundProduct) {
-        return next(new HttpError('COuld not find product for the provided ID',404)) 
-    }
-    res.status(200).json({message:'Product Found',product: foundProduct.toObject({getters: true})})
-}
-
-const productsByUserId = async (req,res,next) => {
-    const userId = req.params.userId
-    let foundProducts
-    
-    try {
-      foundProducts = await Product.find({creator: userId}).exec()
-    } catch (error) {
-      return next(new HttpError('Fetch products failed',500))
-    }
-    
-    if(!foundProducts) {
-        return next(new HttpError('Could not find product for the provided user ID',404))
-    }
-    res.status(200).json({totalProducts: foundProducts.length, products: foundProducts.map(prod=>prod.toObject({getters:true}))})
-}
+  if (!foundProducts) {
+    return next(
+      new HttpError('Could not find product for the provided user ID', 404)
+    );
+  }
+  res
+    .status(200)
+    .json({
+      totalProducts: foundProducts.length,
+      products: foundProducts.map((prod) => prod.toObject({ getters: true })),
+    });
+};
 
 //UPDATE
 
-const updateProduct = async (req,res,next) => {
-
-  const error = validationResult(req)
-  if(!error.isEmpty()) {
-    return next(new HttpError('Invalid inputs, try again',422))
+const updateProduct = async (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return next(new HttpError('Invalid inputs, try again', 422));
   }
-    const productId = req.params.prodId
-    let foundProduct
+  const productId = req.params.prodId;
+  let foundProduct;
 
-    try {
-      foundProduct = await Product.findById(productId).exec()
-    } catch (error) {
-      return next(new HttpError('Could not find product',404))
-    }
-    
-    if(!foundProduct) {
-        throw new HttpError('COuld not find product for the provided ID', 404)
-    }
+  try {
+    foundProduct = await Product.findById(productId).exec();
+  } catch (error) {
+    return next(new HttpError('Could not find product', 404));
+  }
 
-    const { name,description,images,sizes,colors,price,creator } = req.body
+  if (!foundProduct) {
+    throw new HttpError('COuld not find product for the provided ID', 404);
+  }
 
-    foundProduct.name = name
-    foundProduct.description = description
-    foundProduct.images = images
-    foundProduct.sizes = sizes
-    foundProduct.colors = colors
-    foundProduct.price = price
-    foundProduct.creator = creator
+  const { name, description, images, sizes, colors, price, creator } = req.body;
 
-    try {
-      const result = await foundProduct.save()
-    } catch (error) {
-      return next(new HttpError('Could not update place, try again',422))
-    }
+  foundProduct.name = name;
+  foundProduct.description = description;
+  foundProduct.images = images;
+  foundProduct.sizes = sizes;
+  foundProduct.colors = colors;
+  foundProduct.price = price;
+  foundProduct.creator = creator;
 
-
-    res.status(200).json({message:'Product Updated', product: foundProduct.toObject({getters:true})}) 
-}
+  try {
+    const result = await foundProduct.save();
+  } catch (error) {
+    return next(new HttpError('Could not update product, try again', 422));
+  }
+  res
+    .status(200)
+    .json({
+      message: 'Product Updated',
+      product: foundProduct.toObject({ getters: true }),
+    });
+};
 
 //DELETE
 
-const deleteProduct = (req,res,next) => {
-    const productId = req.params.prodId
-    const foundProduct = DUMMY_PRODUCTS.find(prod=>prod.id===productId)
-    if(!foundProduct) {
-      throw new HttpError('Could not find product for the specified ID',400)
-    }
-    DUMMY_PRODUCTS = DUMMY_PRODUCTS.filter(prod=>prod.id!==productId  )
-    res.status(200).json({message: 'Product Deleted'})
-}
+const deleteProduct = async (req, res, next) => {
+  const productId = req.params.prodId;
+  let foundProduct
   
+  try {
+    foundProduct = await Product.findById(productId).exec()
+  } catch (error) {
+    return next(new HttpError('Could not fetch product',500))
+  }
+  
+  if (!foundProduct) {
+    throw new HttpError('Could not find product for the specified ID', 400);
+  }
+  
+  try {
+    await foundProduct.remove()
+  } catch (error) {
+   return next(new HttpError('Could not delete product',500))
+  }
 
-  exports.allProducts = allProducts
-  exports.productsById = productsById
-  exports.productsByUserId = productsByUserId
-  exports.createProduct = createProduct
-  exports.updateProduct = updateProduct
-  exports.deleteProduct = deleteProduct
+  res.status(200).json({ message: 'Product Deleted' });
+};
+
+exports.allProducts = allProducts;
+exports.productsById = productsById;
+exports.productsByUserId = productsByUserId;
+exports.createProduct = createProduct;
+exports.updateProduct = updateProduct;
+exports.deleteProduct = deleteProduct;
