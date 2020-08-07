82229f65a9cee132df0fdec8ab3c0ba3a3cc6a00
FACTS  LIMITED <admin@factsafrica.com>
2020-08-07 07:11:16 +0300

    add validators to email and password upon sign up

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
+const { check } = require('express-validator')
+
 const router = express.Router()
 
 const userControllers = require('../controllers/user-controller')
 
 router.get('/all',userControllers.allUsers)
 router.get('/:userId',userControllers.getUserById)
-router.post('/sign-up',userControllers.signUp)
+router.post('/sign-up',[check('name').isLength({min:3}), check('email').isEmail(),check('password').isLength({min:6})],userControllers.signUp)
 router.post('/login',userControllers.login)
 
 module.exports = router
