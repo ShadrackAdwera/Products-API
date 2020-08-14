43d90cdbcc57db86c1c41f562825fbeb3ddbf3c4
FACTS  LIMITED <admin@factsafrica.com>
2020-08-14 16:16:27 +0300

    change the product model to accept an array of sizes

--- models/products.js
+++ models/products.js
@@ -4,12 +4,7 @@ const productsSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   images: { type: Array, required: true },
-  sizes: {
-    sm: { type: Number, required: true },
-    md: { type: Number, required: true },
-    lg: { type: Number, required: true },
-    xl: { type: Number, required: true },
-  },
+  sizes: { type: Array, required: true},
   colors: [{ type: String, required: true }],
   price: { type: Number, required: true },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
